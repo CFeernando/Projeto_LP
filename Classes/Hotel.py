f82615eb03c1a47efa9e9a632b8767e234b1ce44
Caio Fernando <caaiomelo1003@gmail.com>
2024-07-15 11:43:24 -0300

    Correção das listas e dicionário

--- Classes/Hotel.py
+++ Classes/Hotel.py
@@ -1,8 +1,13 @@
+from Classes.Reserva import *
+
 class Hotel:
     def __init__(self):
         self.nome = "Hotel Paradise"
         self.cnpj = "77.537.261/0001-00"
         self.contato = "hotelparadise@gmail.com"
 
-    def listaReserva(self):
-        self.lista_cadastro = []
+    @staticmethod
+    def listaReserva():
+        adiciona_Reserva = Reserva.lista_Cliente()
+        lista_cadastro = []
+
--- Functions.py
+++ Functions.py
@@ -1,13 +1,17 @@
-from Classes import Reserva
-from Classes import Cliente
+from Classes.Reserva import *
+from Classes.Cliente import *
+from Classes.Hotel import *
+
 def cadastro():
     nome = str(input('Nome: '))
     cpf = str(input('CPF: '))
     contato = str(input('Contato: '))
     id = str(input('ID: '))
     cliente = Cliente(nome, cpf, contato,id)
+    novo = Reserva()
+    novo.fazer_cadastro_cliente(cliente)
 
-    #for cliente in novo.hotel.listaReserva():
-        #print(cliente)
+    for cliente in novo.hotel.listaReserva():
+        print(cliente)
 
 cadastro()
